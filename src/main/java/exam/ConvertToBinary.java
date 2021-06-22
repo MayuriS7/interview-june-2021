b4001aed77b6c44331c12297c8b1b6c546645456
MayuriS7 <62198459+MayuriS7@users.noreply.github.com>
2021-06-22 12:08:11 +0530

    35

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,17 @@
 package exam;
-
+import java.io.*;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    String str;
+    str=Integer.toBinaryString(n);
+     return "str";
+  }
+  public static void main(String[] args)
+  {
+    Scanner sc= new Scanner(System.in);
+    int num=sc.nextInt();
+    System.out.println(toBinary(num));
   }
 }
